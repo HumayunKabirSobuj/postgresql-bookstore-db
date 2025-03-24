0c8e6b607bc233e17c069de28b5d127de0527af3
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-03-24 13:39:12 +0600

    Create Book Table Added

---
+++ PostgreSQL_Assignment.sql
@@ -0,0 +1,11 @@
+-- Active: 1742229780318@@127.0.0.1@5432@bookstore_db@public
+
+-- Create books table
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    price FLOAT4 CHECK (price >= 0),
+    stock INT CHECK (stock >= 0),
+    published_year INT
+);
